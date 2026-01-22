65d3f5a89cb480e2f0ad3a6b7bb5ec3bb545c8ac
Camila Richter <ric23183@uvg.edu.gt>
2026-01-21 18:05:27 -0600

    función para identificar errores individuales

--- task3.py
+++ task3.py
@@ -1,1 +1,17 @@
 #Camila Richter
+
+import math
+
+class Task3:
+    y_real = [100, 150, 200, 250, 300]
+    y_prediccion = [110, 140, 210, 240, 500]
+
+    def calcular_error_individual:
+        n = len(y_real)
+        errores = []
+
+        for i in range(n):
+            error = y_real[i] - y_pred[i]
+            errores.append(error)
+            
+        return y_error
